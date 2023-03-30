fe257eb314a05aad19530c46077fdb39cb965c34
Rene Huanca <renehuanca999@gmail.com>
2023-03-30 11:09:24 -0400

    feat: add the hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useStore } from './hooks/useStore'
+
 function App () {
+  const { fromLanguage, setFromLanguage } = useStore()
+
   return (
     <div className="App">
       <h1>Google translate clone</h1>
+      <button onClick={() => {
+        setFromLanguage('es')
+      }}>Cambiar loading</button>
+      {fromLanguage}
     </div>
   )
 }
---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const SUPPORTED_LANGUAGES = {
+  es: 'English',
+  en: 'Spanish',
+  de: 'Deutsch'
+}
+
+export const AUTO_LANGUAGE = 'auto'
